bcad3515d387de2a800ed87d5fa814cc05a9c614
mageAoe <scq1144282310@163.com>
2023-10-29 22:39:59 +0800

    '更新nextjs'

--- assets/framework_NextJS_index.md.35eb7328.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,O as l}from"./chunks/framework.41379913.js";const F=JSON.parse('{"title":"NextJS","description":"","frontmatter":{},"headers":[],"relativePath":"framework/NextJS/index.md"}'),e={name:"framework/NextJS/index.md"},p=l("",41),o=[p];function t(r,c,i,y,b,u){return n(),a("div",null,o)}const d=s(e,[["render",t]]);export{F as __pageData,d as default};
